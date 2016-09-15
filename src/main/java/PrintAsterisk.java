7575b1511200b899a5dcaca815365d211a2a74ee
liujia <jiaLiu@CNjiaLiu.local>
2016-09-15 19:48:48 +0800

    get one *

--- src/main/java/PrintAsterisk.java
+++ src/main/java/PrintAsterisk.java
@@ -2,4 +2,7 @@
  * Created by jiaLiu on 9/15/16.
  */
 public class PrintAsterisk {
+    public String getAsterisk() {
+        return "*";
+    }
 }
---
+++ src/test/java/printAsteriskTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by jiaLiu on 9/15/16.
+ */
+public class printAsteriskTest {
+
+    @Test
+    public void Should_return_one_asterisk(){
+        PrintAsterisk printAsterisk = new PrintAsterisk();
+
+        assertThat(printAsterisk.getAsterisk(),is("*"));
+    }
+}
