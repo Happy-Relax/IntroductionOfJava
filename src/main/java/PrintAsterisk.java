8bba92dc7db78d0c5f02b2323ce06bb5c8d2050e
liujia <jiaLiu@CNjiaLiu.local>
2016-09-15 18:37:36 +0800

    create project

---
+++ src/main/java/PrintAsterisk.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jiaLiu on 9/15/16.
+ */
+public class PrintAsterisk {
+}
---
+++ src/test/java/firstTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by jiaLiu on 9/15/16.
+ */
+public class firstTest {
+
+    @Test
+    public void first(){
+
+        assertThat(1,is(1));
+    }
+}
