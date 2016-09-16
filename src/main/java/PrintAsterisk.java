2b7374998b4eecdf9c044b475230538e1fe4e8bd
liujia <jiaLiu@CNjiaLiu.local>
2016-09-16 13:50:33 +0800

    print right triangle

--- src/main/java/PrintAsterisk.java
+++ src/main/java/PrintAsterisk.java
@@ -3,12 +3,28 @@
  */
 public class PrintAsterisk {
 
-    public String getAsterisk(int i) {
+    public String getAsterisks(int i) {
         String asterisks = "";
         for (int j = 0; j < i; j++) {
             asterisks += "*";
         }
 
         return asterisks;
+    }
+
+    public String getAsteriskLines(int i) {
+        String lines = "";
+        for (int j = 0; j < i; j++) {
+            lines += getAsterisks(1) + "\n";
+        }
+        return lines;
+    }
+
+    public String getRightTriangle(int i) {
+        String triangle = "";
+        for (int j = 0; j < i; j++) {
+            triangle += getAsterisks(j+1) +"\n";
+        }
+        return triangle;
     }
 }
--- src/test/java/printAsteriskTest.java
+++ src/test/java/printAsteriskTest.java
@@ -12,6 +12,27 @@ public class printAsteriskTest {
     public void Should_return_many_asterisk(){
         PrintAsterisk printAsterisk = new PrintAsterisk();
 
-        assertThat(printAsterisk.getAsterisk(3),is("***"));
+        assertThat(printAsterisk.getAsterisks(3),is("***"));
+    }
+
+    @Test
+    public void Should_return_three_lines_asterisk(){
+        PrintAsterisk printAsterisk = new PrintAsterisk();
+
+        assertThat(printAsterisk.getAsteriskLines(3),is("*\n*\n*\n"));
+    }
+
+    @Test
+    public void Should_return_right_triangle(){
+        PrintAsterisk printAsterisk = new PrintAsterisk();
+
+        assertThat(printAsterisk.getRightTriangle(3),is("*\n**\n***\n"));
+    }
+
+    @Test
+    public void Should_return_isosceles_triangle(){
+        PrintAsterisk printAsterisk = new PrintAsterisk();
+
+        assertThat(printAsterisk.getRightTriangle(3),is("  *\n ***\n*****\n"));
     }
 }
