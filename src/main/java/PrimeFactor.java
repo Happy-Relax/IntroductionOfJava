cf86315a2fa42a57ae08971f73d4424504654e52
liujia <jiaLiu@CNjiaLiu.local>
2016-09-16 16:28:25 +0800

    get prime factor

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by jiaLiu on 9/16/16.
  */
 public class PrimeFactor {
+    public boolean IsPrimeNumber(int number) {
+        double sqrtNumber = Math.ceil(Math.sqrt(number));
+        for (int i = 2; i <= sqrtNumber; i++) {
+            if (number % i == 0 && i != number){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Integer> GetAllFactors(int number) {
+        ArrayList<Integer> factors = new ArrayList<Integer>();
+        double sqrtNumber = Math.ceil(Math.sqrt(number));
+        for (int i=2; i<= sqrtNumber; i++){
+            if(number % i == 0){
+                if(!factors.contains(i)){factors.add(i);}
+                if(!factors.contains(number/i)){factors.add(number/i);}
+            }
+        }
+        return factors;
+    }
+
+    public List<Integer> GetPrimeFactors(int number) {
+        List<Integer> factors = GetAllFactors(number);
+        List<Integer> primeFactors = new ArrayList<Integer>();
+        for (int i = 0; i < factors.size(); i++) {
+            if (IsPrimeNumber(factors.get(i))){
+                primeFactors.add(factors.get(i));
+            }
+        }
+        Collections.sort(primeFactors);
+        return primeFactors;
+    }
 }
--- src/test/java/primeFactorTest.java
+++ src/test/java/primeFactorTest.java
@@ -1,5 +1,44 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by jiaLiu on 9/16/16.
  */
 public class primeFactorTest {
+
+    @Test
+    public void Should_return_true_when_give_an_prime_number(){
+        PrimeFactor primeFactor = new PrimeFactor();
+
+        assertThat(primeFactor.IsPrimeNumber(13),is(true));
+        assertThat(primeFactor.IsPrimeNumber(2),is(true));
+        assertThat(primeFactor.IsPrimeNumber(12),is(false));
+    }
+
+    @Test
+    public void Should_return_all_factors(){
+        PrimeFactor primeFactor = new PrimeFactor();
+
+        List<Integer> factors = primeFactor.GetAllFactors(12);
+        assertThat(factors.size(),is(4));
+        assertThat(factors.contains(2),is(true));
+        assertThat(factors.contains(3),is(true));
+        assertThat(factors.contains(4),is(true));
+        assertThat(factors.contains(6),is(true));
+    }
+
+    @Test
+    public void Should_return_prime_factors_with_sort(){
+        PrimeFactor primeFactor = new PrimeFactor();
+
+        List<Integer> factors = primeFactor.GetPrimeFactors(30);
+        assertThat(factors.size(),is(3));
+        assertThat(factors.get(0),is(2));
+        assertThat(factors.get(1),is(3));
+        assertThat(factors.get(2),is(5));
+    }
 }
